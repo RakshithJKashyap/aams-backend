7dd7ebbe9bcff415a01c0978fb1d77a44410460c
Shravan K G <73581559+bitmos@users.noreply.github.com>
2023-04-24 22:07:03 +0530

    Add files via upload

--- face_detector.py
+++ face_detector.py
@@ -8,8 +8,9 @@ import os
 import cv2
 from scipy.spatial.distance import cosine
 import time
-
+from facenet_pytorch import InceptionResnetV1
 import tensorflow as tf 
+import torch
 
 class Detector:
     model = None
@@ -27,7 +28,7 @@ class Detector:
     def __init__(self) -> None:
         self.model = VGGFace(model='resnet50', include_top=False, input_shape=(224, 224, 3), pooling='avg')
         self.detector = MTCNN()
-
+        self.facenet = InceptionResnetV1(pretrained='vggface2').eval()
     
     def  check_similarity_with_database(self,known_embedding, candidate_embedding, thresh=0.5):
         score = cosine(known_embedding, candidate_embedding)
@@ -36,6 +37,7 @@ class Detector:
     def get_face_crop(self,face, frame):
         x1, y1, width, height = face
         x2, y2 = x1 + width, y1 + height
+        print("Appending")
         self.face_coordiantes.append({   'x1':x1,
                                     'x2':x2,
                                     'y1':y1,
@@ -59,10 +61,20 @@ class Detector:
 
     def extract_face(self,frame):
         results = self.detector.detect_faces(frame)
+        print("Detected faces",len(results))
         for i in range(len(results)):
             self.extract_face_coordinates(results[i]['box'], frame)
-
-    def get_embeddings(self, frame):
+    def get_embeddings_facenet(self,frame):
+        self.current_frame_faces = []
+        self.extract_face(frame)
+        for face in self.current_frame_faces:
+            face = cv2.cvtColor(face, cv2.COLOR_BGR2RGB)
+            face = torch.from_numpy(face).permute(2, 0, 1).float()
+            embedding = self.facenet(face.unsqueeze(0))
+            self.class_face_vectors.append(embedding)
+    
+    def get_embeddings_vggface(self, frame):
+        self.current_frame_faces = []
         self.extract_face(frame) 
         for i in self.current_frame_faces:
             samples = asarray(self.current_frame_faces, 'float32')
